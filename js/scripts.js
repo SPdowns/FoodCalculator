38d19fb732ccbfdf8a9707efd91c19540556c21b
Sean Downs <spdowns@protonmail.com>
2020-06-01 19:16:24 -0700

    cleans up string questions & responses

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,25 @@
 var tempCelToFareh = function(celToFareh) {
   return celToFareh * 1.8 + 32;
 };
-var celToFareh = prompt("Enter a Temperature");
+var celToFareh = prompt("Enter a temperature");
 var tempResult = tempCelToFareh(celToFareh);
 
-alert(tempResult);
+alert(celToFareh + " in celcius is " + tempResult + " in fahrenheit");
 
 var volGalToLit = function(galToLit) {
     return galToLit * 3.7854;
 }
 
-var galToLit = prompt("Enter in Volume");
+var galToLit = prompt("Enter volume in gallons to convert to litres");
 var volResult = volGalToLit(galToLit);
 
-alert(volResult);
+alert(galToLit + " gallons is " + volResult + " litres");
 
-var distanceImptoMet = function(impToMet) {
-    return impToMet * 0.3937008
+var distanceCenToInch = function(cenToInch) {
+    return cenToInch * 0.3937008
 }
 
-var impToMet = prompt("Enter in Distance");
-var distanceResult = distanceImptoMet(impToMet);
+var cenToInch = prompt("Enter distance in CM to convert to Inch");
+var distanceResult = distanceCenToInch(cenToInch);
 
-alert(impToMet + " CM is " + distanceResult + " in IN")
+alert(cenToInch + " centimeters is " + distanceResult + " in American inches")
